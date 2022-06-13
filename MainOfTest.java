a1a0039ed5d27cc89e6be58be8a9266d2a48a655
Pradeep <97354272+Pradeep1040@users.noreply.github.com>
2022-06-13 12:26:40 +0530

    Add files via upload

---
+++ MainOfTest.java
@@ -0,0 +1,12 @@
+package org.jsp.Constructors;
+
+public class MainOfTest 
+{
+	public static void main(String[] args) 
+	{
+		new Test1();
+		new B(100);
+		
+	}
+
+}
---
+++ Test1.java
@@ -0,0 +1,21 @@
+package org.jsp.Constructors;
+
+public class Test1 
+{
+	public Test1()
+	{
+		System.out.println("Hello Test1");
+		//user defined no-arg constractor
+	}
+	
+
+}
+class B
+{
+	public B(int x)
+	{
+		System.out.println(x);
+		// user defined argument Constractor
+	}
+}
+
